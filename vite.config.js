66d8e517b48968133f82cc17efece21bed0c38e9
joe-nontakorn <nontakorn.joe01@gmail.com>
2025-04-21 11:07:13 +0700

    Initial commit

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,25 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
+import { viteStaticCopy } from 'vite-plugin-static-copy'
 
 export default defineConfig({
-  plugins: [react()],
-  base: '/joe-portfolio/',
+  plugins: [
+    react(),
+    viteStaticCopy({
+      targets: [
+        {
+          src: 'public/**/*',
+          dest: '', // จะ copy จาก public มาวางที่ root ของ dist
+        },
+        {
+          src: [
+            'src/assets/styles/**/*',
+         
+          ],
+          dest: 'assets', // จะได้เป็น dist/assets/
+        },
+      ],
+    }),
+  ],
+   base: '/joe-portfolio/', // เอาไว้เปิดใช้ถ้าจะ deploy บน GitHub Pages
 })
