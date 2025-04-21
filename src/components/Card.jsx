b0f0124336c2afd189994ae7e09adc440d917d22
joe-nontakorn <nontakorn.joe01@gmail.com>
2025-04-21 14:46:17 +0700

    Initial commit

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,11 +1,13 @@
 // Card.jsx
 function Card({ title, link, children, className = "" }) {
   return (
-    <div className={`bg-white dark:bg-gray-800 p-4 rounded-lg shadow hover:shadow-lg transition ${className}`}>
+    <div
+      className={`bg-white dark:bg-gray-800 p-4 sm:p-6 md:p-8 rounded-lg shadow-md hover:shadow-lg transition ${className}`}
+    >
       {link && (
         <iframe
           src={link + "#toolbar=0&navpanes=0&scrollbar=0"}
-          className="w-full h-64 mb-4 rounded"
+          className="w-full h-[300px] sm:h-[400px] lg:h-[500px] mb-4 rounded"
         ></iframe>
       )}
 
